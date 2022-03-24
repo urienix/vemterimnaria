c6253c94780b14cd97ae3a29fbb75054c1e77373
urienix <urienix@hotmail.com>
2022-03-24 10:29:21 -0600

    Eliminando etiqueta a de badges

--- src/helpers/handlebars.js
+++ src/helpers/handlebars.js
@@ -9,12 +9,12 @@ module.exports = helpers = {
 
     setEmergencies(value){
         return value === '1' ? `
-        <a href="#" class="nes-badge">
-            <span class="is-success" style="max-width: 50px; margin-left: -20px;">Si</span>
-        </a>` : `
-        <a href="#" class="nes-badge">
-            <span class="is-error" style="max-width: 50px; margin-left: -20px;">No</span>
-        </a>`;
+        <span class="nes-badge">
+            <span class="is-success" style="max-width: 50px; margin-left: -22px;">Si</span>
+        </span>` : `
+        <span class="nes-badge">
+            <span class="is-error" style="max-width: 50px; margin-left: -22px;">No</span>
+        </span>`;
     },
 
     setChecked(option, value){
